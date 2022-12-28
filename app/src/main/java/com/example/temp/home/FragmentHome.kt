11dd1aac6a95c3647d8185dcb744e18d080b4a10
AnCha <tory912@gmail.com>
2022-12-29 02:29:25 +0900

    home 리스트 없는 경우 보이는 이미지 추가

--- app/src/main/java/com/example/temp/home/FragmentHome.kt
+++ app/src/main/java/com/example/temp/home/FragmentHome.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.temp.databinding.FragmentHomeBinding
 
 
@@ -29,6 +30,28 @@ class FragmentHome : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         binding.viewFlipper.flipInterval = 3000  //화면 넘김 간격 메서드 (millisceond)
         binding.viewFlipper.startFlipping()
+
+        val myTourListArray = arrayListOf<MyTourListModel>(
+//            MyTourListModel("프랑스","2023.04.21~2023.05.14"),
+//            MyTourListModel("일본","2023.01.21~2023.01.30"),
+//            MyTourListModel("베트남","2023.03.11~2023.03.22"),
+//            MyTourListModel("미국","2023.01.11~2023.01.14"),
+
+        )
+
+
+        if(myTourListArray.isNullOrEmpty()){
+            //비어있다면
+            binding.notMyHomeList.visibility = View.VISIBLE
+            binding.myHomeList.visibility = View.GONE
+
+        }else{
+            binding.notMyHomeList.visibility = View.GONE
+            binding.myHomeList.visibility = View.VISIBLE
+        }
+
+        binding.myHomeList.layoutManager = LinearLayoutManager(requireContext())
+        binding.myHomeList.adapter = MyTourListAdapter(requireContext(),myTourListArray)
 
         return binding.root
     }
--- app/src/main/java/com/example/temp/home/MyTourListAdapter.kt
+++ app/src/main/java/com/example/temp/home/MyTourListAdapter.kt
@@ -1,6 +1,26 @@
 package com.example.temp.home
 
-class MyTourListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.temp.databinding.ItemTourCardBinding
 
+class MyTourListAdapter(val context: Context, private val myTourListArray: ArrayList<MyTourListModel>): RecyclerView.Adapter<MyTourListAdapter.ListViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(ItemTourCardBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return  myTourListArray.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val currentItem = myTourListArray[position]
+
+        holder.binding.country.text = currentItem.country
+        holder.binding.date.text = currentItem.date
+    }
+    class ListViewHolder(val binding: ItemTourCardBinding) : RecyclerView.ViewHolder(binding.root)
 }
