3955dd659641b693a8ddee26ce3e946ce8d33cd1
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2022-12-28 23:29:42 +0900

    Recommend 부분 완성

---
+++ app/src/main/java/com/example/temp/Recommend/RecommendViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.temp.Recommend
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.temp.R
+
+class RecommendViewPagerAdapter(picList: ArrayList<ViewPagerData>) :
+    RecyclerView.Adapter<RecommendViewPagerAdapter.PagerViewHolder>() {
+    val item = picList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+
+    override fun getItemCount(): Int = item.size
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.pic.setImageResource(item[position].img)
+        holder.country.setText(item[position].country)
+        holder.detail.setText(item[position].detail)
+    }
+
+    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.recommend_viewpager, parent, false)){
+        val pic: ImageView = itemView.findViewById<ImageView>(R.id.imageView_pic)
+        val country: TextView = itemView.findViewById(R.id.country)
+        val detail: TextView = itemView.findViewById(R.id.detail)
+    }
+}
---
+++ app/src/main/java/com/example/temp/Recommend/ViewPagerData.kt
@@ -0,0 +1,7 @@
+package com.example.temp.Recommend
+
+data class ViewPagerData(
+    var img: Int,
+    var country: String,
+    var detail: String
+)
