ec97673396ece2f6fe919c1a788c70cb7099a81d
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2022-12-29 04:55:33 +0900

    Recommend 리스트 부분 미완

--- app/src/main/java/com/example/temp/RecomListActivity.kt
+++ app/src/main/java/com/example/temp/RecomListActivity.kt
@@ -2,30 +2,58 @@ package com.example.temp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.SingleRequest
 import com.example.temp.databinding.ActivityRecomListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+
 
 class RecomListActivity : AppCompatActivity() {
 
     lateinit var recomListAdapter: RecomListAdapter
-    val binding by lazy { ActivityRecomListBinding.inflate(layoutInflater) }
+    lateinit var binding : ActivityRecomListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recom_list)
-        initRecycler()
+        binding = ActivityRecomListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    }
+        // 텍스트 변경
+        val country = intent.getStringExtra("country")
+        binding.textView2.text = country
+        Log.d("test", "생성")
 
-    private fun initRecycler(){
-        val list = ArrayList<checkboxData>()
-        list.add(checkboxData(title="에펠탑",check = false))
-        list.add(checkboxData(title="에펠탑",check = false))
-        list.add(checkboxData(title="에펠탑",check = false))
-        list.add(checkboxData(title="에펠탑",check = false))
-        list.add(checkboxData(title="에펠탑",check = false))
+        val list = doTask("https://travel.naver.com/overseas/JP294232/country/topPlace/tspot")
 
+//        Log.d("test", list.get(0).title)
+        // 리사이클러뷰
         recomListAdapter= RecomListAdapter(list)
         binding.rvRecomList.adapter=recomListAdapter
         recomListAdapter.notifyDataSetChanged()
+    }
+
+    private fun doTask(url: String) : ArrayList<checkboxData> {
+        var itemList : ArrayList<checkboxData> = arrayListOf()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val document = Jsoup.connect(url).get()
+            val elements : Elements = document.select("ul[class=topPlaceSpot_list__1l27A]").select("li")
+
+            elements.forEach { element ->
+                var img = element.select("figure[class=poiItem_thumb__1JJu8]").select("img").attr("src")
+                var spot = element.select("figure[class=poiItem_thumb__1JJu8]").select("img").attr("alt")
+                var detail = element.select("span[class=poiItem_desc__31sC4]").text()
+
+                var item = checkboxData(img, spot, detail)
+                itemList.add(item)
+            }
+        }
+
+        return itemList
     }
 }
--- app/src/main/java/com/example/temp/Recommend/RecommendViewPagerAdapter.kt
+++ app/src/main/java/com/example/temp/Recommend/RecommendViewPagerAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.temp.Recommend
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.temp.R
+import com.example.temp.RecomListActivity
 
 class RecommendViewPagerAdapter(picList: ArrayList<ViewPagerData>) :
     RecyclerView.Adapter<RecommendViewPagerAdapter.PagerViewHolder>() {
@@ -18,10 +22,17 @@ class RecommendViewPagerAdapter(picList: ArrayList<ViewPagerData>) :
     override fun getItemCount(): Int = item.size
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        val obj = item[position]
         holder.pic.setImageResource(item[position].img)
         holder.country.setText(item[position].country)
         holder.detail.setText(item[position].detail)
 
+        holder.pic.clipToOutline = true
+        holder.pic.setOnClickListener{
+            val intent = Intent(holder.itemView.context, RecomListActivity::class.java)
+            intent.putExtra("country",holder.country.text)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
